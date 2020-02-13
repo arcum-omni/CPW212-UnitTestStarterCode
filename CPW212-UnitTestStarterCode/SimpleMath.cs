2c1ac1b4d73fd4a18ab7f8ef215d51a9e4990f16
arcum-omni <travis.l.eiler@gmail.com>
2020-02-12 17:22:20 -0800

    Add project files.

---
+++ CPW212-UnitTestStarterCode/SimpleMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CPW212_UnitTestStarterCode
+{
+    /*****************************************************************************
+     * 
+     * YOU MAY REFACTOR THE PARAMETER NAMES BUT DO NOT CHANGE THE METHOD SIGNATURES
+     * 
+     * ***************************************************************************/
+    public static class SimpleMath
+    {
+        public static double Add(double augend, double addend)
+        {
+            double sum = augend + addend;
+            return sum;
+        }
+
+        public static double Subtract(double minuend, double subtrahend)
+        {
+            double difference = minuend - subtrahend;
+            return difference;
+        }
+
+        public static double Multiply(double multiplicand, double multiplier)
+        {
+            double product = multiplicand * multiplier;
+            return product;
+        }
+
+        public static double Divide(double numerator, double denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero!");
+            }
+            double quotient = numerator / denominator;
+            return quotient;
+        }
+    }
+}
---
+++ CPW212-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CPW212_UnitTestStarterCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW212_UnitTestStarterCode.Tests
+{
+    [TestClass()]
+    public class SimpleMathTests
+    {
+        [TestMethod()]
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
+        {
+            // Arrange => Act
+            double sum = SimpleMath.Add(num1, num2);
+
+            // Assert
+            Assert.AreEqual(num1 + num2, sum);
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            // Arrange => Act
+            double num1 = 0.0;
+            double num2 = 10.0;
+            double sum1 = SimpleMath.Multiply(num1, num2);
+
+            double num3 = 2.0;
+            double num4 = 20.0;
+            double sum2 = SimpleMath.Multiply(num3, num4);
+
+            double num5 = 1000000.0;
+            double num6 = 2000000.0;
+            double sum3 = SimpleMath.Multiply(num5, num6);
+
+            // Assert
+            Assert.AreEqual(num1 * num2, sum1);
+            Assert.AreEqual(num3 * num4, sum2);
+            Assert.AreEqual(num5 * num6, sum3);
+        }
+
+        [TestMethod]
+        public void Divide_DenominatorZero_ThrowsArgumentException()
+        {
+            // Arrange => Act
+            int numerator = 10;
+            int denominator = 0;
+
+            // Assert
+            Assert.ThrowsException<DivideByZeroException>(() => SimpleMath.Divide(numerator, denominator));
+        }
+
+        #region TestMethod
+        [TestMethod]
+        [DataRow(0.0, 10.0)]
+        [DataRow(1.0, 10.0)]
+        [DataRow(10.0, 10.0)]
+        [DataRow(100.0, 10.0)]
+        [DataRow(1, 0.000000001)]
+        #endregion
+        public void Divide_TwoNumbers_ReturnQuotient(double numerator, double denominator)
+        {
+            // Arrange => Act
+            double quotient = SimpleMath.Divide(numerator, denominator);
+
+            // Assert
+            Assert.AreEqual(numerator/denominator, quotient);
+        }
+
+        #region TestMethod
+        [TestMethod]
+        [DataRow(10.0, 5.0)]
+        [DataRow(100.0, 0.0)]
+        [DataRow(-1.0, -10.0)]
+        [DataRow(0.0, -0.0)]
+        #endregion
+        public void Subtract_TwoNumbers_ReturnDifference(double minuend, double subtrahend)
+        {
+            // Arrange => Act
+            double difference = SimpleMath.Subtract(minuend, subtrahend);
+
+            // Assert
+            Assert.AreEqual(minuend - subtrahend, difference);
+        }
+    }
+}
